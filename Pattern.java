6f2182c02bfe721cdb5c090df3966f70602b61d8
Amruta Kanikbhat <soniyakangune@gmail.com>
2022-10-01 16:42:39 +0530

    Her am push my java code file on server side

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+package TestPack;
+public class Pattern
+{
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+//Using git load on server side.
+	}
+
+}
